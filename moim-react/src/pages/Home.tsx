ed2f97d034a15eba47570661a5b459034b13dcbe
오현택 <duduio2050@gmail.com>
2023-02-06 23:43:27 +0900

    수정중

---
+++ moim-react/src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import '../App.css';
+
+class Main extends Component {
+    render() {
+        return (
+            <div className="App-body">
+                <div className="Header">
+                    <p>오현택님을 위한 추천 클럽!</p>
+                    <p><i className="icon-heart"></i>설정한 관심사에 맞춰 추천 드려요</p>
+                </div>
+                <div className="Category">
+
+                </div>
+                <div></div>
+                <div></div>
+            </div>
+
+        );
+    }
+
+}
+
+export default Main;
